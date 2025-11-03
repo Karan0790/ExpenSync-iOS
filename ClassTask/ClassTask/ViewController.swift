6e0349aca5ae2a09e82d840f74fef19fc6e7889f
Hargun Singh <hargunsingh@Harguns-MacBook-Air-2.local>
2025-11-03 10:10:41 +0530

    Base App

---
+++ ClassTask/ClassTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassTask
+//
+//  Created by Hargun Singh on 03/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
